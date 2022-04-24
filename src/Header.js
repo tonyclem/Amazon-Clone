b6ccbb8b9e9ad92c846b464e35a539f1a0a52b9e
tonyclem <tonyclem001@gmail.com>
2022-04-24 17:14:38 +0200

    added checkout component

--- src/Header.js
+++ src/Header.js
@@ -2,15 +2,18 @@ import React from "react";
 import "./Header.css";
 import SearchIcon from "@mui/icons-material/Search";
 import ShoppingBasketIcon from "@mui/icons-material/ShoppingBasket";
+import { Link } from "react-router-dom";
 
 function Header() {
   return (
     <div className="header">
-      <img
-        className="header__logo"
-        src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
-        alt="amazon logo"
-      />
+      <Link to="/">
+        <img
+          className="header__logo"
+          src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
+          alt="amazon logo"
+        />
+      </Link>
 
       <div className="header__search">
         <input className="header__searchInput" type="text" />
@@ -33,12 +36,14 @@ function Header() {
           <span className="header__optionLineTwo">Prima</span>
         </div>
 
-        <div className="header__optionBasket">
-          <ShoppingBasketIcon />
-          <span className="header__optionLineTwo header__basketCount">
-            {/* {basket?.length} */}0
-          </span>
-        </div>
+        <Link to="/checkout">
+          <div className="header__optionBasket">
+            <ShoppingBasketIcon />
+            <span className="header__optionLineTwo header__basketCount">
+              {/* {basket?.length} */}0
+            </span>
+          </div>
+        </Link>
       </div>
     </div>
   );
--- src/Home.js
+++ src/Home.js
@@ -2,9 +2,10 @@ import React from "react";
 import "./Home.css";
 import Product from "./Product";
 
+
 function Home() {
   return (
-    <div className="home">
+      <div className="home">
       <div className="home__container">
         <img
           className="home__image"
