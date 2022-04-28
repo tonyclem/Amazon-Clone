e0c732185d65d4f5e49c213be9987d2b3164f44f
tonyclem <tonyclem001@gmail.com>
2022-04-28 14:55:48 +0200

    added sign in section with styles

---
+++ src/Login.js
@@ -0,0 +1,71 @@
+import React, { useState } from "react";
+import "./Login.css";
+import { Link } from "react-router-dom";
+
+function Login() {
+  // useState is a hook that allows us to create a state variable
+  const [email, setEmail] = useState(""); // email state, useStats is always set to an empty parentheses
+  const [password, setPassword] = useState(""); // password state
+
+  const signIn = (event) => {
+    event.preventDefault(); // prevents the page from refreshing by default
+    console.log("sign in with email and password");
+  }
+
+  const register = (event) => {
+    event.preventDefault();
+
+  }
+
+  return (
+    <div className="login">
+      <Link to="/">
+        <img
+          className="login__logo"
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/1024px-Amazon_logo.svg.png"
+        />
+      </Link>
+
+      <div className="login__container">
+        <h1>Sign in</h1>
+        <form>
+          <h5>E-mail</h5>
+          {/* the values get the values type in the input,  */}
+          <input
+            type="text"
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
+          />
+
+          <h5>Password</h5>
+          {/* get the password value,  */}
+          <input
+            type="password"
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+          />
+
+          <button
+            type="submit"
+            onClick={signIn}
+            className="login__signInButton"
+          >
+            Sign In
+          </button>
+        </form>
+
+        <p>
+          By signing-in you agree to the AMAZON FAKE CLONE Conditions of Use &
+          Sale. Please see our Privacy Notice, our Cookies Notice and our
+          Interest-Based Ads Notice.
+        </p>
+
+        <button onClick={register} className="login__registerButton">
+          Create your Amazon account
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
