216b88456a5f0f8754fd1049d313d7b0a2f94278
tonyclem <tonyclem001@gmail.com>
2022-04-28 21:46:50 +0200

    modified checkout section

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,23 +1,21 @@
-import React from 'react';
-import Subtotal from './Subtotal';
-import { useStateValue } from './StateProvider';
-import CheckoutProduct from './CheckoutProduct';
-import './Checkout.css';
+import React from "react";
+import Subtotal from "./Subtotal";
+import { useStateValue } from "./StateProvider";
+import CheckoutProduct from "./CheckoutProduct";
+import "./Checkout.css";
 function Checkout() {
   const [{ basket, user }, dispatch] = useStateValue();
   return (
     <div className="checkout">
-       <div className="checkout__left">
-       <img
+      <div className="checkout__left">
+        <img
           className="checkout__ad"
           src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg"
           alt=""
         />
         <div>
-          
-          <h2 className="checkout__title">
-            Your Shopping Basket
-          </h2>
+          <h3>{user ? user.email : ""}</h3>
+          <h2 className="checkout__title">Your Shopping Basket</h2>
           {/* Basket Item */}
 
           {basket.map((item) => (
@@ -29,17 +27,14 @@ function Checkout() {
               rating={item.rating}
             />
           ))}
-          </div>
-       </div>
-
-       <div className="checkout__right">
-        <Subtotal />
+        </div>
       </div>
 
-
-
+      <div className="checkout__right">
+        <Subtotal />
+      </div>
     </div>
-  )
+  );
 }
 
-export default Checkout
+export default Checkout;
