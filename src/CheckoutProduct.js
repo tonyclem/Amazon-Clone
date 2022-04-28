ce15f363c6781bdbb0fa07c3ac537fe0c6fb8854
tonyclem <tonyclem001@gmail.com>
2022-04-28 13:41:26 +0200

    added more functionality on reducer and checkoutProduct in oder to remove items from basket

--- src/CheckoutProduct.js
+++ src/CheckoutProduct.js
@@ -1,7 +1,18 @@
 import React from "react";
 import "./CheckoutProduct.css";
+import { useStateValue } from "./StateProvider";
 
 function CheckoutProduct({ id, image, title, price, rating, hideButton }) {
+  const [{ basket }, dispatch] = useStateValue(); // get the basket from the state
+
+  const removeFromBasket = () => {
+    // Remove item from basket
+    dispatch({
+      type: "REMOVE_FROM_BASKET",
+      id: id,
+    });
+  }
+
   return (
     <div className="checkoutProduct">
       <img className="checkoutProduct__image" src={image} alt="product" />
@@ -20,7 +31,7 @@ function CheckoutProduct({ id, image, title, price, rating, hideButton }) {
         </div>
         <button
           className="checkoutProduct__remove"
-          onClick={() => hideButton(id)}
+          onClick={removeFromBasket}
         >
           Remove from Basket{" "}
         </button>
--- src/reducer.js
+++ src/reducer.js
@@ -3,7 +3,7 @@ export const initialState = {
 };
 
 export const getBasketTotal = (basket) =>
-  basket.reduce((amount, item) => item.price + amount, 0);
+  basket.reduce((amount, item) => item.price + amount, 0); // sum the price of the basket
 
 const reducer = (state, action) => {
     console.log(action);
@@ -14,7 +14,29 @@ const reducer = (state, action) => {
       return {
         ...state,
         basket: [...state.basket, action.item],
-      };
+      }; // get the current state and add the item to the basket, then return the new state
+
+    case "REMOVE_FROM_BASKET": // go through the basket id and which item matches the id,
+      const index = state.basket.findIndex(
+        (basketItem) => basketItem.id === action.id
+      ); // findIndex is a method that will return the index of the item that matches the action.id
+
+      // i copy the state basket into new variable newBasket
+      let newBasket = [...state.basket]; // create a new basket array called newBasket
+
+      if (index >= 0) {
+        newBasket.splice(index, 1); // splice will remove the item from the basket
+      } else {
+        console.warn(
+          `Cant remove product (id: ${action.id}) as its not in the basket!`
+        );
+      }
+
+      return {
+        ...state,
+        basket: newBasket,
+      }; // return the new state
+
     default:
       return state;
   }
